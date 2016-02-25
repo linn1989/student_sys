71a7608efda6666b4f4b7f1e7b6ff2be7fc5e646
YX <yangxing20107@gmail.com>
2016-02-25 17:07:12 +0800

    x z

--- project/X/mysql.py
+++ project/X/mysql.py
@@ -1,8 +1,31 @@
-import  cymysql
+'''
+ mysql DB 
+'''
 
-con=cymysql.connect(host='localhost',passwd='root',db='yx',user='root')
-cur=con.cursor()
+# *args[]= procedureName + IN , write different SQL by dirrerent procedureName.
+def mysql(*args): 
+	import  cymysql
+	con=cymysql.connect(host='localhost',passwd='root',db='yx',user='root')
+	cur=con.cursor()
 
-cur.execute('call ')
+	# call procedure 
+	if args[0]=='signin1':
+		procedure_= "(%d)" % (args[1],) 
 
-res=cur.fetchall()
+	if args[0]=='signin2':    #### '%s' must add the '' around %s
+		procedure_= "(%d,'%s','%s')" % (args[1],args[2],args[3],) 		
+
+	# commit SQL
+	procedure="call " + args[0] + procedure_
+	cur.execute(procedure)
+	con.commit()
+
+	# return res
+	if cur.fetchall()!=None:
+		res=cur.fetchall()
+	else:
+		res=''
+	# close connection
+	cur.close()
+	con.close()
+	return res
--- project/X/views.py
+++ project/X/views.py
@@ -18,7 +18,15 @@ def signin(request):
 	return render(request,'signin.html')
 # sign ok page
 def signin_ok(request):	
-	id=request.GET['id']
+	id=int(request.GET['id'])
 	passwd=request.GET['passwd']
-	return render(request,'signin_ok.html',{'id':id,'passwd':passwd})
+	
+	import mysql
+	res=mysql.mysql('signin1',id)
+	if len(res)==1: # id already exists
+		return render(request,'signin_fail.html',{'id':id})
+	elif len(res)==0:
+		mysql.mysql('signin1',id,passwd,'test')
+		return render(request,'signin_ok.html',{'id':id,'passwd':passwd})
+	
 
